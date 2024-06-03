dff74508b09403fb20c5fa1e9213fb229154541d
Rigoberto09 <rigobertoborjas3@gmail.com>
2024-06-03 14:05:26 -0600

    validaciones de datos, funcion reciclable para metodos, put y post

--- src/cliente/cliente.service.ts
+++ src/cliente/cliente.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotAcceptableException } from '@nestjs/common';
 import { Cliente } from './cliente.entity/cliente.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ClienteInterface } from './cliente.interface/cliente.interface.interface';
 
 @Injectable()
 export class ClienteService {
@@ -9,13 +10,59 @@ export class ClienteService {
     private readonly clienteRepository: Repository<Cliente>) { }
 
     //control de datos vacios Clientes
+    async verificacionDatosEnvio(identify: number, id: string, DatosCliente: ClienteInterface) {
+        let DatosFaltantes = '';
+    
+        if (!DatosCliente.name || DatosCliente.name.trim() === '') {
+          DatosFaltantes += 'Nombre completo';
+        }
+    
+        if (!DatosCliente.direcciones || DatosCliente.direcciones.toString().trim() === '') {
+          DatosFaltantes += DatosFaltantes ? ', Dirección' : 'Dirección';
+        }
+    
+        if (!DatosCliente.phone || DatosCliente.phone.toString().trim() === '') {
+          DatosFaltantes += DatosFaltantes ? ', Teléfono' : 'Teléfono';
+        }
+    
+        if (DatosFaltantes === '') {
+          if (identify === 0) {
+            return this.crearCliente(DatosCliente);
+          } else if (identify === 1) {
+            // return this.actualizarCliente(id, DatosCliente);
+          } else if (identify === 2) {
+            // return this.eliminarCliente(id);
+          }
+        } else {
+          throw new NotAcceptableException('Faltan datos: ' + DatosFaltantes);
+        }
+      }
+    async crearCliente(NuevoCliente:ClienteInterface):Promise<ClienteInterface>{
+        try {
+            const cliente = this.clienteRepository.save(NuevoCliente);
+            return cliente;
+            
+        } catch (error) {
+            // console.log('erro al crear cliente '+ error);
+            throw new InternalServerErrorException('Error al crear cliente');
+        }
+    }
 
     async getCliente(): Promise<Cliente[]> {
-        const cliente = await this.clienteRepository.find()
-        if (cliente.length == 0) {
+        try {
+            const cliente = await this.clienteRepository.find()
+            if (cliente.length == 0) {
+                throw new NotAcceptableException('No se encontraron datos en el registro');
+    
+            }
+            return cliente;
+            
+        } catch (error) {
+            // console.log('no se puede mostrar los clientes' + error)
             throw new NotAcceptableException('No se encontraron datos en el registro');
-
+    
         }
-        return cliente;
     }
+    
+
 }
