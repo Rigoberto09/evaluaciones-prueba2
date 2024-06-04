10d856a73583eb0c522791f8d51f42386eaa7963
Rigoberto09 <rigobertoborjas3@gmail.com>
2024-06-04 10:18:28 -0600

    Desarrollo de interface de tipado de direcciones

--- src/direcciones/direcciones.interface/direcciones.interface.interface.ts
+++ src/direcciones/direcciones.interface/direcciones.interface.interface.ts
@@ -1,1 +1,11 @@
-export interface DireccionesInterface {}
+import { Cliente } from "src/cliente/cliente.entity/cliente.entity";
+
+export interface DireccionesInterface {
+    addresCode:number;
+    
+    calle:string;
+    
+    ciudad:string
+    
+    cliente:Cliente;
+}
